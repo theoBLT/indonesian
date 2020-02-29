1310b3358e043f70b526521cfbfb9f0186f35a7e
[theoBLT] <[theobloc@gmail.com]>
2020-02-29 23:13:32 +0000

    A sentence was missing a dot ¯\_(ツ)_/¯

--- src/components/header.js
+++ src/components/header.js
@@ -7,9 +7,9 @@ function HeaderTemplate (props) {
     <div className ="header">
       <h1 className="title">{text}</h1>
       <h1 className="title translated">{translation}</h1>
-      
+
       <p className="subtitle">2 kelas seminggu. 10 kata bahasa Indonesia baru setiap kali.</p>
-      <p className="subtitle translated">2 classes a week. 10 new Indonesian words each time</p>
+      <p className="subtitle translated">2 classes a week. 10 new Indonesian words each time.</p>
     </div>
   )
 }
