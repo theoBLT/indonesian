cffde0680c374fea68e1e02294fd2da5226343af
[theoBLT] <[theobloc@gmail.com]>
2020-06-17 00:00:32 +0100

    Fix: duplicate image pat in gatsby-source-filesystem config

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,23 +12,23 @@ module.exports = {
     {
       resolve:`gatsby-source-filesystem`,
       options: {
-        name:`images`,
-        path: `${__dirname}/src/images/`
-      }
+        name:`pages`,
+        path: `${__dirname}/src/pages`
+      },
     },
     {
       resolve:`gatsby-source-filesystem`,
       options: {
         name:`posts`,
-        path: `${__dirname}/src/posts/`
-      }
+        path: `${__dirname}/src/posts`
+      },
     },
     {
       resolve:`gatsby-source-filesystem`,
       options: {
         name:`images`,
-        path: `${__dirname}/src/images/`
-      }
+        path: `${__dirname}/src/images`
+      },
     },
     {
       resolve:`gatsby-source-airtable`,
