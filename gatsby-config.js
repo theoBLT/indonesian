14dd7da50e985ecc4af8f05b9667824d4b94160e
[theoBLT] <[theobloc@gmail.com]>
2020-06-16 23:55:23 +0100

    Basic markdown-based blog setup with MDX files

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,21 +13,21 @@ module.exports = {
       resolve:`gatsby-source-filesystem`,
       options: {
         name:`images`,
-        path: `${__dirname}/src/images`
+        path: `${__dirname}/src/images/`
       }
     },
     {
       resolve:`gatsby-source-filesystem`,
       options: {
         name:`posts`,
-        path: `${__dirname}/src/posts`
+        path: `${__dirname}/src/posts/`
       }
     },
     {
       resolve:`gatsby-source-filesystem`,
       options: {
         name:`images`,
-        path: `${__dirname}/src/images`
+        path: `${__dirname}/src/images/`
       }
     },
     {
@@ -70,9 +70,9 @@ module.exports = {
           resolve: `gatsby-remark-images`,
           options: {
             maxWidth:1200,
-          }
-        }
-      ]
+          },
+        },
+      ],
       }
     }
   ],
