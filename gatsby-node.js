f79d39f4c398825c4f41f69ff97f701ff2884348
[theoBLT] <[theobloc@gmail.com]>
2020-06-17 21:43:37 +0100

    Prep work to create pages programmatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,43 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// exports.createPages = async function({actions,graphql}){
+//     const {data} = await graphql`
+//         query {
+//             allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+//                 edges {
+//                   node {
+//                     frontmatter {
+//                       slug
+//                     }
+//                     id
+//                   }
+//                 }
+//               }
+//         }
+//     `
+//     // Create paginated pages for posts
+//     const postsPerPage = 2;
+//     const numPages = Math.ceil(data.allMdx.edges.length / postsPerPage);
+    
+//     Array.from({ length: numPages}).forEach((_,i) => {
+//         actions.createPages({
+//             path: i === 0 ? `/list` : `/list-${i + 1}`,
+//             component: require.resolve("./src/templates/allPosts.js"),
+//             context: {
+//                 limit: postsPerPage,
+//                 skip: i * postPerPage,
+//                 numPages,
+//                 currentPage: i + 1,
+//             }
+//         })
+//     })
 
-// You can delete this file if you're not using it
+//     // Create single blog posts
+//     data.allMdx.edges.forEach(edge => {
+//         const slug = edge.node.frontmatter.slug;
+//         const id = edge.node.id;
+//         actions.createPages({
+//             path: slug,
+//             component: require.resolve(`./src/templates/singlePost.js`),
+//             context: {id},
+//         })
+//     }) 
+// }
