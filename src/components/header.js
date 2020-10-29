dae2017ea98d147b67e3db5ecaf7fb0d6b3905a9
[theoBLT] <[theobloc@gmail.com]>
2020-10-29 20:52:26 +0000

    [edit] Changed header text

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ function HeaderTemplate (props) {
       <h1 className="title">{text}</h1>
       <h1 className="title translated">{translation}</h1>
 
-      <p className="subtitle">Each week, I get to learn 10 new words with my Indonesian teacher, recapped here for personal convenience.</p>
+      <p className="subtitle">My goal is to be fluent in Indonesian by end of 2021. Each week, I spend 3 hours learning Indonesian with <a href="https://cintabahasa.com/teacher-and-staff/viona.html">Viona</a> from <a href="https://cintabahasa.com/">Cinta Bahasa</a>, and recap the words I learn below.</p>
     </div>
   )
 }
