831e005ac2de37caba6a219ea7a9a0cf16f5f5b1
[theoBLT] <[theobloc@gmail.com]>
2020-10-29 20:41:49 +0000

    [Fix] Removed unused query in main layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,33 +7,22 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
-    <>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-      </div>
-    </>
-  )
+      <>
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0 1.0875rem 1.45rem`,
+          }}
+        >
+          <main>{children}</main>
+        </div>
+      </>
+    )
 }
 
 Layout.propTypes = {
