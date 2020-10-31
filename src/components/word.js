3518f14cc3a6f0c07e1af2e4c292d652dadbf59f
[theoBLT] <theobloc@gmail.com>
2020-10-31 21:48:27 +0000

    [Feat] Now examples show on click

--- src/components/word.js
+++ src/components/word.js
@@ -1,14 +1,16 @@
-import React from "react" 
+import React, {useState} from "react"
 import {capitalize} from "../utils/helpers.js"
 
 export default props =>  {
     const {word, type, translation, key, example} = props;
+    const [visible, setVisible] = useState(false);
+    console.log(visible);
     return (
-        <li key={key}>
+        <li key={key} class={example? "clickable" :""}onClick={() => setVisible(!visible)}>
             <span className="word">{capitalize(word)}</span> 
             <span className="type">({type})</span> 
             <span className="translation">{capitalize(translation)}</span>
-            {example? <span className="example">{example}</span> : null}
+            {example? <span className={visible? "example" : "example hidden"}>{example}</span> : null}
       </li>
     )
 }
