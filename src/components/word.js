e76ddd7a1d78e65f72dc1585b3f55773a143090d
[theoBLT] <theobloc@gmail.com>
2020-11-01 06:57:39 +0000

    [Fix] Removed console debugging event

--- src/components/word.js
+++ src/components/word.js
@@ -4,7 +4,6 @@ import {capitalize} from "../utils/helpers.js"
 export default props =>  {
     const {word, type, translation, key, example} = props;
     const [visible, setVisible] = useState(false);
-    console.log(visible);
     return (
         <li key={key} class={example? "clickable" :""}onClick={() => setVisible(!visible)}>
             <span className="word">{capitalize(word)}</span> 
