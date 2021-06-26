ed4e9a52b8994d59848ce8f5a9daaf89206bd321
[theoBLT] <theobloc@gmail.com>
2021-06-26 22:28:09 +0100

    Cosmetic fixes

--- src/components/header.js
+++ src/components/header.js
@@ -10,8 +10,8 @@ function HeaderTemplate(props) {
         <img src={Logo} alt="CelotehBahsa's logo" />
       </Link>
       <div className="menubar">
-        <Link to="/">Home</Link> | <Link to="/books">Book Club</Link> |
-        <Link to="/causes"> Causes </Link>
+        <Link to="/">Home</Link> | <Link to="/books">Book Club</Link> |&nbsp;
+        <Link to="/causes">Causes </Link>
       </div>
       <div id="intro" className={short ? `hidden` : ``}>
         <div id="avatar">
@@ -23,10 +23,12 @@ function HeaderTemplate(props) {
 
         <div id="welcome_text">
           <p className="subtitle">
-            Hi, I'm <a href="https://www.twitter.com/theoblochet">Théo</a>. My
-            goal is to speak Indonesian fluently by the end of 2021. I study the
-            language for 3 hours a week with my teacher Viona and recap what I
-            learn below. <Link to="/celoteh/tentang/kenapa">Why?</Link>
+            Hi, and welcome to my site! I'm{" "}
+            <a href="https://www.twitter.com/theoblochet">Théo</a>. My goal is
+            to speak Indonesian fluently by the end of 2021 (see{" "}
+            <Link to="/celoteh/tentang/kenapa">why</Link>). I study the language
+            for 3 hours a week with my teacher Viona and recap what I learn
+            here.
           </p>
         </div>
       </div>
