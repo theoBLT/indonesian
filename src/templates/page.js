91376c0452d12cef189a7c0bf31c9c8abce92e44
[theoBLT] <theobloc@gmail.com>
2021-07-01 10:24:36 +0100

    Removed big header from articles, added breakdcrumbs

--- src/templates/page.js
+++ src/templates/page.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Header from "../components/header"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
 export default function Template({ data }) {
   const { markdownRemark } = data
@@ -12,10 +13,12 @@ export default function Template({ data }) {
     <>
       <SEO title={frontmatter.title} description={excerpt} />
       <Layout>
-        <Header />
+        <Header short="true" />
         <div className="article">
+          <Link to="/">Go back</Link>
           <h3>{frontmatter.title}</h3>
           <div dangerouslySetInnerHTML={{ __html: html }} />
+          <Link to="/">Go back</Link>
         </div>
       </Layout>
     </>
