779462ba5fba1523aa1b607e754482d16bca6ef7
Theo Blochet <tb@stripe.com>
2021-12-07 14:29:24 +0700

    Topbar color for safari

--- src/components/seo.js
+++ src/components/seo.js
@@ -67,6 +67,10 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name:`theme-color`,
+          content:`#803234`
+        }
       ].concat(meta)}
     />
   )
