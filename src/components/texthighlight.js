34be9fce55542556992b5b8b1570875e52c87fa3
Theo Blochet <tb@stripe.com>
2021-12-18 09:50:33 +0700

    Comments and cleanup

--- src/components/texthighlight.js
+++ src/components/texthighlight.js
@@ -6,6 +6,9 @@ function Texthighlight(props) {
   
   splitWords.map((word) => console.log(word))
 
+// TODO: Highlighting is broken in at least 2 ways, and will need fixing:
+// 1. When the word to highlight contains a space, like `gelar tikar`
+// 2. When the word is immediately followed by punctuation, like `tikar.`
   
   const highlightedWords = splitWords.map((word) => word.toUpperCase() === highlight.toUpperCase()? `<span class ="example-highlight">${word}</span>` : word)
   const joinedWords = highlightedWords.join(' ')
