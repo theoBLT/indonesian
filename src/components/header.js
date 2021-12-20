f155012d1a9cbacb60a99282bcbcf5e75330c192
Theo Blochet <tb@stripe.com>
2021-12-20 11:46:20 +0700

    removed unused props from header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
-function HeaderTemplate(props) {
-  const { short } = props
+function HeaderTemplate() {
   return (
     <div className="header">
       <Link to="/">
