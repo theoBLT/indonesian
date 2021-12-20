e03f3881cbb9b3c611184525c5e950e9b4b92a07
Theo Blochet <tb@stripe.com>
2021-12-20 13:07:18 +0700

    Search moved to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,58 @@
 import React from "react"
 import { Link } from "gatsby"
+import algoliasearch from 'algoliasearch/lite';
+import Searchhit from "../components/searchhit"
+import { InstantSearch, SearchBox, Hits, connectStateResults } from 'react-instantsearch-dom';
+
+const searchClient = algoliasearch('KNGW4E76GQ', '303d6ed1f7af0462583d12f07a871e04');
+
+const Results = connectStateResults(({ searchState, searchResults, children }) => (
+  // If there is a state and a query
+  searchState && searchState.query ? ( 
+
+  // If that query returned zero results
+    searchResults && searchResults.nbHits === 0 ? 
+    <div className="no-results">
+      There are no results for <span className="example-highlight">{searchState.query}</span>, would you like to <a href={`https://translate.google.com/?sl=id&tl=en&text=`+searchState.query+`&op=translate`}>Google translate it</a>, or <a target="_blank" href={`https://airtable.com/shrV656H340zjGWi4?prefill_word=`+searchState.query}>add this word?</a>
+      </div> 
+      : children) 
+  
+  // Then, if there is a query, results, but no result returned
+  : (
+     null
+    // <div className="font-size--small"><i>Learners before you searched for <Link to="kepepet">kepepet</Link>, <Link to="gokil">gokil</Link> or <Link to="gelar-tikar">gelar tikar</Link>.</i> To add a word, <a href="https://airtable.com/shrV656H340zjGWi4" target="_blank"> head here.</a></div>
+  ) 
+));
+
 
 function HeaderTemplate() {
   return (
+    <InstantSearch 
+          searchClient={searchClient} 
+          indexName="Words"
+          >
     <div className="header">
       <Link to="/">
         CB.
       </Link>
+
+
+          <SearchBox 
+          translations={{
+            placeholder: 'Search any word...',
+          }}/>
+
       <div className="menubar">
         <Link to="about">About</Link>
       </div>
     </div>
+    <div className="container">
+    <Results>
+            <Hits hitComponent={Searchhit}/>
+    </Results>
+    </div>
+    </InstantSearch>
+  
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,12 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
+import { graphql} from "gatsby"
 import Layout from "../components/layout"
 import Header from "../components/header"
 import Concepts from "../components/concepts"
 import Session from "../components/session"
-import Searchhit from "../components/searchhit"
 import WordOfTheDay from "../components/wordoftheday"
 import SEO from "../components/seo"
 import Footer from "../components/footer"
-import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, Hits, connectStateResults } from 'react-instantsearch-dom';
-
-const searchClient = algoliasearch('KNGW4E76GQ', '303d6ed1f7af0462583d12f07a871e04');
-
-const Results = connectStateResults(({ searchState, searchResults, children }) => (
-  // If there is a state and a query
-  searchState && searchState.query ? ( 
-
-  // If that query returned zero results
-    searchResults && searchResults.nbHits === 0 ? 
-    <div className="no-results">
-      There are no results for {searchState.query}, would you like to <a href={`https://translate.google.com/?sl=id&tl=en&text=`+searchState.query+`&op=translate`}>Google translate it</a>, or <a target="_blank" href={`https://airtable.com/shrV656H340zjGWi4?prefill_word=`+searchState.query}>add this word?</a>
-      </div> 
-      : children) 
-  
-  // Then, if there is a query, results, but no result returned
-  : (
-    <div className="font-size--small"><i>Learners before you searched for <Link to="kepepet">kepepet</Link>, <Link to="gokil">gokil</Link> or <Link to="gelar-tikar">gelar tikar</Link>.</i> To add a word, <a href="https://airtable.com/shrV656H340zjGWi4" target="_blank"> head here.</a></div>
-  ) 
-));
 
 export default ({ data }) => {
   return (
@@ -36,20 +14,6 @@ export default ({ data }) => {
       <SEO />
       <Header />
       <Layout>
-        <InstantSearch 
-          searchClient={searchClient} 
-          indexName="Words"
-          >
-
-          <SearchBox 
-          translations={{
-            placeholder: 'Search for any word...',
-          }}/>
-
-          <Results>
-            <Hits hitComponent={Searchhit}/>
-          </Results>
-        </InstantSearch>
        
         <div class ="grid-row">
         <Concepts title="From the blog" />
