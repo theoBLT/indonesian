6db2b53808146a742263a8d895face4989b7fd40
[theoBLT] <theobloc@gmail.com>
2022-05-23 23:15:59 +0200

    [Add] Updated SEO about page for new site purpose

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,31 +10,32 @@ export default () => {
     <>
       <SEO title="My journey learning Indonesian" />
       <Header />
-      <Layout> 
-    
+      <Layout>
         <div id="intro">
-        <div id="avatar">
-          <img
-            src="https://pbs.twimg.com/profile_images/1322835228197556224/jJwEyHUX_400x400.png"
-            alt="A picture of Théo Blochet"
-          />
-        </div>
+          <div id="avatar">
+            <img
+              src="https://pbs.twimg.com/profile_images/1322835228197556224/jJwEyHUX_400x400.png"
+              alt="A picture of Théo Blochet"
+            />
+          </div>
 
-        <div id="welcome_text">
-          <p className="subtitle">
-            Hi, and welcome to my site! I'm{" "}
-            <a href="https://www.twitter.com/theoblochet">Théo</a>. My goal is
-            to speak Indonesian fluently by the end of 2021 (see{" "}
-            <Link to="/celoteh/tentang/kenapa">why</Link>). I study the language
-            for 3 hours a week with my teacher Viona and recap what I learn
-            here.
-          </p>
+          <div id="welcome_text">
+            <p className="subtitle">
+              Hi, and welcome to Celoteh Bahasa! I'm{" "}
+              <a href="https://www.twitter.com/theoblochet">Théo</a>. I've been
+              learning Indonesian since 2019 (see{" "}
+              <Link to="/celoteh/tentang/kenapa">why</Link>), and recap the
+              "Bahasa gaul" words that I learn in this small dictionary.{" "}
+            </p>
+            <p className="subtitle">
+              <a href="https://www.twitter.com/theoblochet">Hit me up</a> on
+              Twitter if this helped in any way, or if you have any feedback or
+              ideas for the site!
+            </p>
+          </div>
         </div>
-      </div> 
         <Footer />
       </Layout>
     </>
   )
 }
-
-
