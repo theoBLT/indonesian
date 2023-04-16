a6d088a1572c0315b490ec89e7117b18ac7418db
[theoBLT] <theobloc@gmail.com>
2023-04-16 17:31:42 +0200

    [Fix] Highlight now covers all use-cases for definitions

--- src/components/texthighlight.js
+++ src/components/texthighlight.js
@@ -1,24 +1,19 @@
 import React from "react"
-import sanitizeHtml from 'sanitize-html';
+import sanitizeHtml from "sanitize-html"
 
 function Texthighlight(props) {
   const { highlight, paragraph } = props
-  const splitWords = paragraph.split(' ')
 
-// TODO: Highlighting is broken in at least 2 ways, and will need fixing:
-// 1. When the word to highlight contains a space, like `gelar tikar`
-// 2. When the word is immediately followed by punctuation, like `tikar.`
-  
-  const highlightedWords = splitWords.map((word) => word.toUpperCase() === highlight.toUpperCase()? `<span class ="example-highlight">${word}</span>` : word)
-  const joinedWords = highlightedWords.join(' ')
-  const cleanHtml = sanitizeHtml(joinedWords,{allowedTags: ['span'],allowedAttributes: {
-    span: [ 'class']}});
+  const regex = new RegExp(`${highlight}(?=[.,!?;:]|\\s|$)`, "gi")
+  const highlightedParagraph = paragraph.replace(
+    regex,
+    `<span class="example-highlight">$&</span>`
+  )
 
   return (
-      <>
-      <span dangerouslySetInnerHTML={{__html: cleanHtml}} >
-      </span>
-      </>
+    <>
+      <span dangerouslySetInnerHTML={{ __html: highlightedParagraph }}></span>
+    </>
   )
 }
 
